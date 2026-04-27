f15a28e250bd3c12b23350fff615945284602ded
SagitaKDX <leminhl1412@gmail.com>
2026-04-27 15:41:00 +0700

    chore: physically delete all dead files from repo and working tree
    Removed from disk and git:
    - Data/ (duplicate docs)
    - config/ (unused settings module)
    - utils/ (audio_enhance, audio_utils, ring_buffer, wake_word, __init__)
    - core/audio_input, noise_reduction, silero_vad, speaker_isolation, vad
    - Root test/debug scripts (10 files) + pect

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# Voice Assistant v2 - Utils Package
-from .audio_utils import *
-from .ring_buffer import RingBuffer
-# from .wake_word import WakeWordDetector  # Uncomment when implemented
-
-__all__ = [
-    "RingBuffer",
-    # "WakeWordDetector",
-]
